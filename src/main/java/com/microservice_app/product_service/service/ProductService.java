3ca1d778734c173a0c65f6522111cbb412b69b7e
CHIẾN HUỲNH MINH <huynhchien293@gmail.com>
2024-10-10 20:40:12 +0700

    feat: Init product service

--- src/main/java/com/microservice_app/product_service/service/ProductService.java
+++ src/main/java/com/microservice_app/product_service/service/ProductService.java
@@ -28,7 +28,6 @@ public class ProductService {
 
     public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
-
         return products.stream().map(this::mapToProductResponse).toList();
     }
 
